e92dfce3451e010567623998e1873fd655733c90
Love <love.lindqvist101@gmail.com>
2021-09-10 13:16:44 +0200

    Adds basic phoneNumber parsing

--- src/main/java/model/Contact.java
+++ src/main/java/model/Contact.java
@@ -3,8 +3,8 @@ package model;
 public class Contact {
 
     private String name;
-    private String phoneNumber;
-    private String address;
+    private String phoneNumber = "";
+    private String address = "";
 
     Contact(String name){
 
@@ -31,9 +31,21 @@ public class Contact {
     }
 
     public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+        this.phoneNumber = parsePhoneNumber(phoneNumber);
     }
 
+
+    private String parsePhoneNumber(String phoneNumber){
+        StringBuilder sb = new StringBuilder();
+        char[] phoneNumberChars = phoneNumber.toCharArray();
+        for (Character c : phoneNumberChars){
+            if (Character.isDigit(c))
+                sb.append(c);
+            else if (c =='+' || c==' ')
+                sb.append(c);
+        }
+        return sb.toString();
+    }
 
     public void setAddress(String address) {
         this.address = address;
