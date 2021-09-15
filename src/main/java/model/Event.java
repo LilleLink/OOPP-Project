6ecfa15e178b8a52bc08108079383eece142d5cd
Simon Engström <simoneng@student.chalmers.se>
2021-09-15 17:03:33 +0200

    Adds contact list to event.

--- src/main/java/model/Event.java
+++ src/main/java/model/Event.java
@@ -1,6 +1,8 @@
 package model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Event {
 
@@ -9,11 +11,14 @@ public class Event {
     private final LocalDate date;
     private final String description;
 
-    public Event(String name, String address, LocalDate date, String description) {
+    private final List<Contact> registeredContacts;
+
+    public Event(String name, String address, LocalDate date, String description, List<Contact> registeredContacts) {
         this.name = name;
         this.address = address;
         this.date = date;
         this.description = description;
+        this.registeredContacts = registeredContacts;
     }
 
     public String getName() {
@@ -21,7 +26,7 @@ public class Event {
     }
 
     public Event setName(String name) {
-        return new Event(name, this.address, this.date, this.description);
+        return new Event(name, this.address, this.date, this.description, this.registeredContacts);
     }
 
     public String getAddress() {
@@ -29,7 +34,7 @@ public class Event {
     }
 
     public Event setAddress(String address) {
-        return new Event(this.name, address, this.date, this.description);
+        return new Event(this.name, address, this.date, this.description, this.registeredContacts);
     }
 
     public LocalDate getDate() {
@@ -37,7 +42,7 @@ public class Event {
     }
 
     public Event setDate(LocalDate date) {
-        return new Event(this.name, this.address, date, this.description);
+        return new Event(this.name, this.address, date, this.description, this.registeredContacts);
     }
 
     public String getDescription() {
@@ -45,6 +50,7 @@ public class Event {
     }
 
     public Event setDescription(String description) {
-        return new Event(this.name, this.address, this.date, description);
+        return new Event(this.name, this.address, this.date, description, this.registeredContacts);
     }
+
 }
