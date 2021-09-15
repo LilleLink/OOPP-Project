4c2f6eea778f8f2cbf0454b1a2e9c2ab4888c635
Axel Larsson <axoxelol_01@hotmail.com>
2021-09-15 17:14:17 +0200

    Creates a test class for address

--- src/test/java/model/AddressTest.java
+++ src/test/java/model/AddressTest.java
@@ -1,5 +1,13 @@
 package model;
 
-public class AddressTest {
+import model.Address;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
+public class AddressTest {
+    @Test
+    public void TestCleanString(){
+        Address address = new Address("   Hubben     2.1  ");
+        assertEquals("Hubben 2.1", address.getAddress());
+    }
 }
