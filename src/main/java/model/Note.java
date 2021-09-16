6df6370c329ad1d92066c74976d01e3506dc73fd
Simon Johnsson <simzter@outlook.com>
2021-09-16 15:42:14 +0200

    Adds class Note and implements methods for editing and viewing text as well as viewing date and time.

---
+++ src/main/java/model/Note.java
@@ -0,0 +1,68 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+/**
+ * Represents a documented note containing text and the point in time that it was created
+ */
+public class Note {
+
+    private String text;
+
+    private final LocalDateTime pointOfCreation;
+    /**
+     * Creates an instance of Note containing default values as if it were empty
+     *
+     * Sets the text to an empty String and the time of creation to the current time
+     */
+    public Note() {
+        this.text = "";
+        this.pointOfCreation = LocalDateTime.now();
+    }
+
+    /**
+     * Creates an instance of Note containing the given text
+     * Sets the text to an empty String and the time of creation to the current time
+     * @param text the string to be stored
+     */
+    public Note(String text) {
+        this.text = text;
+        this.pointOfCreation = LocalDateTime.now();
+    }
+
+    /**
+     * Gives the text contained in the note
+     * @return a String
+     */
+    public String viewNote() {
+        return this.text;
+    }
+
+    /**
+     * Sets the text contained in the note to the given text
+     * @param newText a new String
+     * @return the given String
+     */
+    public String editNote(String newText) {
+        this.text = newText;
+        return newText;
+    }
+
+    /**
+     * Gives the date of creation
+     * @return a date consisting of year, month and day
+     */
+    public LocalDate viewDate() {
+        return pointOfCreation.toLocalDate();
+    }
+
+    /**
+     * Gives the time of creation
+     * @return a time consisting of hours, minutes, seconds and nanoseconds
+     */
+    public LocalTime viewTime() {
+        return pointOfCreation.toLocalTime();
+    }
+}
