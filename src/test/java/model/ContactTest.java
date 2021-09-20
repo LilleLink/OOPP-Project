2f749465bf1881d58f225d225641c27b6588de80
Axel Larsson <axoxelol_01@hotmail.com>
2021-09-20 15:26:24 +0200

    Removes dated tests after design change

--- src/test/java/model/ContactTest.java
+++ src/test/java/model/ContactTest.java
@@ -16,25 +16,5 @@ public class ContactTest {
         c1 = new Contact(testName);
     }
 
-    @Test
-    public void createTest(){
-        c1 = new Contact(testName);
-        assert testName.equals(c1.getName());
-    }
 
-    @Test
-    public void changeNameTest(){
-        String newName = "new name";
-        Contact c2 = c1.setName(newName);
-        assert !(c1.getName().equals(newName));
-        assert !(c1.equals(c2));
-    }
-
-    @Test
-    public void changePhoneNumber(){
-        String newNumber = "new name";
-        Contact c2 = c1.setPhoneNumber(newNumber);
-        assert !(c1.getPhoneNumber().equals(newNumber));
-        assert !(c1.equals(c2));
-    }
 }
