59cb4c0602a8262d29c06be793fc8b0d50acdf29
Axel Larsson <axoxelol_01@hotmail.com>
2021-09-20 09:54:36 +0200

    Removing UUID from contact and most of user methods as we are refactoring it.

--- src/main/java/model/Contact.java
+++ src/main/java/model/Contact.java
@@ -3,28 +3,22 @@ package model;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.UUID;
 
 public class Contact {
 
     private String name;
     private String phoneNumber = "";
-    private final UUID id;
-    private final ArrayList<UUID> tagsIdList = new ArrayList<UUID>();
 
     /**
-     * Creates a contact with the given name and assigns a UUID to it.
      * @param name The contact's name.
      */
     Contact(String name){
         this.name = name;
-        this.id = UUID.randomUUID();
     }
 
     private Contact(Contact old){
         name = old.name;
         phoneNumber = old.phoneNumber;
-        id = old.id;
     }
 
     /**
@@ -43,13 +37,6 @@ public class Contact {
         return phoneNumber;
     }
 
-    /**
-     *
-     * @return The contact's UUID.
-     */
-    public UUID getId(){
-        return id;
-    }
 
     /**
      * Sets the name of the contact.
@@ -71,36 +58,6 @@ public class Contact {
         Contact newContact = new Contact(this);
         newContact.phoneNumber = number;
         return newContact;
-    }
-
-    /**
-     * Adds a tag ID to the contact's tag id list.
-     * @param id The id of the tag to add
-     * @return If the operation was successful, will return false if contact already has tag.
-     */
-    public boolean addTag(UUID id){
-        if (!tagsIdList.contains(id)){
-            tagsIdList.add(id);
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Removes a tag from the contact.
-     * @param id The id of the tag to remove
-     * @return If the operation was successful, will return false if contact did not have the given tag.
-     */
-    public boolean removeTag(UUID id){
-        return tagsIdList.remove(id);
-    }
-
-    /**
-     *
-     * @return A copy of the contact's tags' ids as an ArrayList.
-     */
-    public ArrayList<UUID> getTagsIdList(){
-        return new ArrayList<>(tagsIdList);
     }
 
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -11,7 +11,6 @@ import java.util.UUID;
 public class User {
     //TODO fix javadoc, rushing to get runnable version W3
     private String name;
-    private final Map<UUID, Contact> contactMap = new HashMap<>();
     private final ArrayList<Event> eventList = new ArrayList<>();
     private final TagFactory tagFactory = new TagFactory();
 
@@ -21,86 +20,6 @@ public class User {
 
     public void setName(String name){
         this.name = name;
-    }
-
-    public void addContact(String name){
-        Contact contact = new Contact(name);
-        contactMap.put(contact.getId(), contact);
-    }
-
-    public String getContactName(UUID id){
-        return contactMap.get(id).getName();
-    }
-
-    public String getContactPhoneNumber(UUID id){
-        return contactMap.get(id).getPhoneNumber();
-    }
-
-    public void setContactName(UUID id, String name){
-        Contact updatedContact = contactMap.get(id).setName(name);
-        contactMap.replace(id, updatedContact);
-    }
-
-    public void setContactPhoneNumber(UUID id, String phoneNumber){
-        Contact updatedContact = contactMap.get(id).setPhoneNumber(phoneNumber);
-        contactMap.replace(id, updatedContact);
-    }
-
-    public boolean createTag(String name){
-        if (!tagFactory.nameIsAvailable(name)) return false;
-        tagFactory.createTag(name);
-        return true;
-    }
-
-    public ArrayList<UUID> getContactTags(UUID contact){
-        return contactMap.get(contact).getTagsIdList();
-    }
-
-    //todo check if contact actually exists for add and remove tag methods
-    public boolean addContactTag(UUID contactId, UUID tagId){
-        Contact contact = contactMap.get(contactId);
-        return(contact.addTag(tagId));
-    }
-
-    public boolean removeContactTag(UUID contactId, UUID tagId){
-        Contact contact = contactMap.get(contactId);
-        return(contact.removeTag(tagId));
-    }
-
-    public ArrayList<UUID> getContactTagIds(UUID contactId){
-        Contact contact = contactMap.get(contactId);
-        return contact.getTagsIdList();
-    }
-
-    //todo check if contact actually exists for add and remove tag methods
-    public boolean addEventTag(Event event, UUID tagId){
-        return(event.addTag(tagId));
-    }
-
-    public boolean removeEventTag(Event event, UUID tagId){
-        return(event.removeTag(tagId));
-    }
-
-    public ArrayList<UUID> getEventTagIds(Event event){
-        return event.getTagsIdList();
-    }
-
-    public Event createEvent(String name, LocalDateTime dateTime){
-        Event event = new Event(name, dateTime);
-        eventList.add(event);
-        return event;
-    }
-
-    public void removeEvent(Event event){
-        eventList.remove(event);
-    }
-
-    public void addContactEvent(UUID contact, Event event) {
-        event.addContact(contact);
-    }
-
-    public ArrayList<Event> getEvents(){
-        return new ArrayList<>(eventList);
     }
 
 
