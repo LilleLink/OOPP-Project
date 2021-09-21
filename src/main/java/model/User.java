07eba2b7e0cf8c841a736c4edd9a287640681111
Simon Engström <simoneng@student.chalmers.se>
2021-09-21 10:16:03 +0200

    Adds javadoc to user class.

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -11,26 +11,53 @@ public class User {
     private final ArrayList<Event> eventList = new ArrayList<>();
     private final TagFactory tagFactory = new TagFactory();
 
+    /***
+     * Instantiates a user object with the specified name.
+     * @param name the name of the user
+     */
     public User(String name){
         this.name = name;
     }
 
-    public void setName(String name){
+    /***
+     * Sets the name of the user
+     * @param name new name of the user
+     */
+    void setName(String name){
         this.name = name;
     }
 
+    /***
+     * Adds an event to the event list
+     * @param event the event to be added
+     * @return true if the operation was successful, false if not
+     */
     boolean addEvent(Event event) {
         return eventList.add(event);
     }
 
+    /***
+     * Removes an event from the eventlist
+     * @param event the event to be removed
+     * @return true if the operation was successful, false if not
+     */
     boolean removeEvent(Event event) {
         return eventList.remove(event);
     }
 
+    /***
+     * Returns the users list of events
+     * @return the list of events
+     */
     public ArrayList<Event> getEventList() {
         return eventList;
     }
 
+    /***
+     * Returns a list of events that the given contact is tagged in
+     * @param contact the subject contact
+     * @return an arraylist of the events the contact is a part of
+     */
     public List<Event> getContactEvents(Contact contact) {
         List<Event> contactEvents= new ArrayList<>();
         for (Event e: eventList) {
