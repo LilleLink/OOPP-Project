30195a143174db81142998669c7f0b2409d7dfe7
Love <love.lindqvist101@gmail.com>
2021-09-21 10:00:21 +0200

    Changes NameNotAvailableException extension to Exception

--- src/main/java/model/NameNotAvailableException.java
+++ src/main/java/model/NameNotAvailableException.java
@@ -2,7 +2,7 @@ package model;
 
 import javax.naming.NameAlreadyBoundException;
 
-public class NameNotAvailableException extends NameAlreadyBoundException {
+public class NameNotAvailableException extends Exception {
 
     NameNotAvailableException(String name){
         super("The name " + name + " was not available");
