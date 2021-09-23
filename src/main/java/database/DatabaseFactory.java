a6f23f3accc8cd59f4482a2da90a238205fb4414
Petter Blomkvist <voxedg@gmail.com>
2021-09-23 10:33:18 +0200

    Changes database service to singleton.

--- src/main/java/database/DatabaseFactory.java
+++ src/main/java/database/DatabaseFactory.java
@@ -1,12 +1,16 @@
 package database;
 
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Optional;
 
 /***
  * The PRMDatabaseFactory contains a static method for creating an abstract PRM database.
  */
 public class DatabaseFactory {
+
+    static private Database database = null;
 
     /***
      * Create a new abstract PRM database.
@@ -14,8 +18,11 @@ public class DatabaseFactory {
      * @return The new PRM database.
      */
     static public Database getDatabase() throws IOException {
-        /// TODO Don't use a temporary file dummy!
-        return new Database(Files.createTempFile("prm-database", ""), new JSONDatabase(), new JSONDatabase());
+        if (database == null) {
+            /// TODO Don't use a temporary file dummy!
+            database = new Database(Files.createTempFile("prm-database", ""), new JSONDatabase(), new JSONDatabase());
+        }
+        return database;
     }
 
 }
