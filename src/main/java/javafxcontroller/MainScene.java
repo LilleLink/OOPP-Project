f44c95397b2f1b8104e3fa9713f785214f2ddeb3
Simon Engström <simoneng@student.chalmers.se>
2021-09-29 15:56:49 +0200

    Adds rough MVCA-architecture. Not quite done since the DB-save/load functionality is not done yet.

---
+++ src/main/java/javafxcontroller/MainScene.java
@@ -0,0 +1,10 @@
+package javafxcontroller;
+
+public class MainScene extends JavaFXComponent{
+
+    public MainScene() {
+        super();
+        System.out.println("MainScene hehe");
+    }
+
+}
---
+++ src/main/java/javafxcontroller/SecondaryScene.java
@@ -0,0 +1,10 @@
+package javafxcontroller;
+
+public class SecondaryScene extends JavaFXComponent{
+
+    public SecondaryScene() {
+        super();
+        System.out.println("yooo secondary scene");
+    }
+
+}
