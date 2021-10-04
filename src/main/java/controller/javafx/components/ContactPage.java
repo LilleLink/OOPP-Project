fafd5436221d2bfdf1582b59e77bbe88e4052167
Axel Larsson <axoxelol_01@hotmail.com>
2021-10-04 13:55:10 +0200

    Adds a button and textField to create new contact, does not currently create a contact. Need to get model access to controller and make the user observable, will bring up during meeting.

--- src/main/java/controller/javafx/components/ContactPage.java
+++ src/main/java/controller/javafx/components/ContactPage.java
@@ -2,6 +2,9 @@ package controller.javafx.components;
 
 import controller.javafx.ViewComponentFactory;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import model.Contact;
@@ -13,18 +16,27 @@ import java.util.List;
 public class ContactPage extends ViewComponent {
     @FXML private AnchorPane baseAnchorPane;
     @FXML private FlowPane cardFlowPane;
+    @FXML private TextField newContactNameTextField;
+    @FXML private Button newContactButton;
     List<Contact> contacts = new ArrayList<>();
 
     public ContactPage(List<Contact> contacts){
         super();
         this.contacts = contacts;
+        this.newContactButton.setOnMouseClicked(this::newContact);
         drawAllCards();
     }
 
-    public void drawAllCards(){
+    private void drawAllCards(){
         for (Contact contact : contacts){
             cardFlowPane.getChildren().add(ViewComponentFactory.CreateContactCard(contact).getPane());
         }
+    }
+
+    private void newContact(MouseEvent mouseEvent){
+        System.out.println(newContactNameTextField.getCharacters());
+        newContactNameTextField.clear();
+        //todo contact user
     }
 
 }
