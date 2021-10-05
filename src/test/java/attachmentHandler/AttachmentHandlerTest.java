a71c46831e3b9123499c6f5c9e657b9cea7d2f1f
Axel Larsson <axoxelol_01@hotmail.com>
2021-10-05 23:28:42 +0200

    Removes temporary test

--- src/test/java/attachmentHandler/AttachmentHandlerTest.java
+++ src/test/java/attachmentHandler/AttachmentHandlerTest.java
@@ -11,23 +11,5 @@ import java.util.UUID;
 
 public class AttachmentHandlerTest {
     IAttachmentHandlerFacade fileHandler = AttachmentHandlerFactory.getService();
-
-    @Test
-    public void testTest(){
-        UUID id = UUID.randomUUID();
-        try {
-            fileHandler.saveAttachment(id, Paths.get("C:/Users/axoxe/dev/java-devel/testCopy.txt"));
-            fileHandler.saveAttachment(id, Paths.get("C:/Users/axoxe/dev/java-devel/testCopy.txt"), "test" );
-            for (Path file : fileHandler.getAttachmentNames(id)){
-                System.out.println(file);
-            }
-            fileHandler.removeId(id);
-            for (Path file : fileHandler.getAttachmentNames(id)){
-                System.out.println(file);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    //TODO: write permanent tests
 }
