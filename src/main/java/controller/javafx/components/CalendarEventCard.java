b8586a1751b74cb4c3e95658ac476bdcf8aaae1d
Simon Engström <simoneng@student.chalmers.se>
2021-10-05 17:41:13 +0200

    Adds CalendarEventCards. Not integrated with calendar yet.

---
+++ src/main/java/controller/javafx/components/CalendarEventCard.java
@@ -0,0 +1,31 @@
+package controller.javafx.components;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import model.Event;
+
+public class CalendarEventCard extends ViewComponent {
+
+    @FXML private AnchorPane calendarEventAnchorPane;
+    @FXML private Label eventNameLabel;
+    @FXML private Label eventTimeLabel;
+    @FXML private Label eventAddressLabel;
+
+    private Event event;
+
+    public CalendarEventCard(Event event) {
+        this.event = event;
+        setLabels();
+    }
+
+    private void setLabels() {
+        eventNameLabel.setText(event.getName());
+        eventTimeLabel.setText(event.getDateTime().toLocalTime().toString());
+        eventAddressLabel.setText(event.getAddress());
+
+    }
+
+
+}
