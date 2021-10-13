7f29c479a1d67d9def1c98cec84a0f4641c0e795
Love <love.lindqvist101@gmail.com>
2021-10-13 12:24:04 +0200

    Adds Javadoc

--- src/main/java/vcf/VCFParser.java
+++ src/main/java/vcf/VCFParser.java
@@ -4,30 +4,49 @@ import model.Contact;
 import model.ContactList;
 import model.exceptions.NameNotAllowedException;
 
-import javax.naming.Name;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Scanner;
 
+
+/**
+ * Class that reads a file or directory and adds contacts to a given list
+ */
 public class VCFParser {
 
     private final ContactList contacts;
 
+    /**
+     * Creates a new parser
+     * @param contactList the ContactList where the new contacts will be added
+     */
     public VCFParser(ContactList contactList){
         this.contacts = contactList;
     }
 
+    /**
+     * Adds a contact from a {*.vcf} file to the list given in the constructor
+     * @param path the path to the vcf file
+     * @throws IOException if the file was not a {*.vcf} file or if an error occurs in {@link Scanner}
+     * @throws NameNotAllowedException if a card with an illegal name is read
+     */
     public void addContact(Path path) throws IOException, NameNotAllowedException{
         if (isVCFFile(path)){
             readContact(path);
         } else {
-            throw new IOException();
+            throw new FileNotFoundException();
         }
     }
 
+    /**
+     * adds contacts from all vcf files in a directory (not recursive)
+     * @param directory the directory to be read
+     * @throws IOException if no {*.vcf} file is found or if an error occurs in {@link Scanner}
+     */
     public void addContactsFromDirectory(Path directory) throws IOException {
+        //FIXME make recursive?
         int createdContacts = 0;
         for (Path path: Files.newDirectoryStream(directory)){
             if (isVCFFile(path)){
