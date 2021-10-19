d9b76b9d058ea0aca252dde53fad98bf9d1a164c
Simon Johnsson <simzter@outlook.com>
2021-10-19 10:44:21 +0200

    Adds JavaDoc for Notifications package classes and interfaces

--- src/main/java/model/notifications/ChronologicalBroadcaster.java
+++ src/main/java/model/notifications/ChronologicalBroadcaster.java
@@ -5,10 +5,15 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * A class responsible for broadcasting {@link IChronological} objects with a given interval.
+ *
+ * @param <T>
+ * @author Simon Johnsson
+ */
 public class ChronologicalBroadcaster<T extends IChronological> implements IObjectBroadcaster<T>, Runnable {
 
     private final List<T> content;
-
     private final List<IObjectBroadcastListener<T>> listeners;
 
     ChronologicalBroadcaster(List<T> content) {
@@ -17,15 +22,28 @@ public class ChronologicalBroadcaster<T extends IChronological> implements IObje
         this.listeners = new ArrayList<>();
     }
 
+    /**
+     * Removes all content that has passed the current time.
+     */
     private void removePassed() {
-        content.removeIf(timeable -> timeable.compareTime(LocalDateTime.now()) < 0);
+        content.removeIf(this::hasPassed);
+    }
+
+    /**
+     * Checks if a chronological object has passed the current time.
+     *
+     * @param chronological the object to check
+     * @return true if the time has passed - false if not
+     */
+    private boolean hasPassed(IChronological chronological) {
+        return chronological.compareTime(LocalDateTime.now()) < 0;
     }
 
     private void broadcastContent() {
         for(Iterator<T> iterator = content.iterator(); iterator.hasNext();) {
-            T timeable = iterator.next();
-            if(0 <= timeable.compareTime(LocalDateTime.now())) {
-                broadcast(timeable);
+            T chronological = iterator.next();
+            if(0 <= chronological.compareTime(LocalDateTime.now())) {
+                broadcast(chronological);
                 iterator.remove();
             }
         }
--- src/main/java/model/notifications/Notifications.java
+++ src/main/java/model/notifications/Notifications.java
@@ -3,6 +3,16 @@ package model.notifications;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A class responsible for collecting objects sent by a {@link ChronologicalBroadcaster}.
+ *
+ * Notifications can be retrieved, cleared and muted.
+ *
+ * In order for the {@code Notifications} to run in the background, it needs to be given to a separate {@link Thread}.
+ *
+ * @param <T> the type of chronological objects to collect
+ * @author Simon Johnsson
+ */
 public class Notifications<T extends IChronological> implements IObjectBroadcastListener<T>, Runnable{
 
     private final List<T> active;
@@ -14,8 +24,12 @@ public class Notifications<T extends IChronological> implements IObjectBroadcast
     }
 
 
-    void clear() {
+    synchronized void clear() {
         active.clear();
+    }
+
+    synchronized void mute(long milliseconds) throws InterruptedException {
+        Thread.sleep(milliseconds);
     }
 
     List<T> getActive() {
