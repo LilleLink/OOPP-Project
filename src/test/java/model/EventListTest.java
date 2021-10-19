8f6eab710e02a2a03e16eaf61da4a64651e0d57b
Axel Larsson <axoxelol_01@hotmail.com>
2021-10-19 15:38:42 +0200

    Creates some basic tests for eventList

--- src/test/java/model/EventListTest.java
+++ src/test/java/model/EventListTest.java
@@ -1,4 +1,53 @@
 package model;
 
+import mocks.MockObserver;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import static org.junit.Assert.assertEquals;
+
 public class EventListTest {
+    private EventList eventList;
+    private final MockObserver mockObserver = new MockObserver();
+
+
+    @Before
+    public void before() {
+        if (!Objects.isNull(eventList)) {
+            eventList.unSubscribe(mockObserver);
+        }
+        eventList = new EventList();
+        eventList.subscribe(mockObserver);
+        mockObserver.resetEventCount();
+    }
+
+    @Test
+    public void addContactTest() {
+        assertEquals(0, eventList.getList().size());
+        eventList.addEvent("test", LocalDateTime.now());
+        assertEquals(1, eventList.getList().size());
+    }
+
+    @Test
+    public void removeContact() {
+        Event event = new Event("test", LocalDateTime.now());
+        assertEquals(0, eventList.getList().size());
+        eventList.addEvent(event);
+        assertEquals(1, eventList.getList().size());
+        eventList.removeEvent(event);
+        assertEquals(0, eventList.getList().size());
+    }
+
+    @Test
+    public void observerTest() {
+        Event event = new Event("test", LocalDateTime.now());
+        assertEquals(0, mockObserver.getEventCount());
+        eventList.addEvent(event);
+        assertEquals(1, mockObserver.getEventCount());
+        eventList.removeEvent(event);
+        assertEquals(2, mockObserver.getEventCount());
+    }
 }
