ce88c1e73a359f0e0d5762853282d43932c08fde
Love <love.lindqvist101@gmail.com>
2021-10-19 10:05:28 +0200

    Fixes error cause

--- src/main/java/model/exceptions/TagNotFoundException.java
+++ src/main/java/model/exceptions/TagNotFoundException.java
@@ -2,6 +2,6 @@ package model.exceptions;
 
 public class TagNotFoundException extends Exception {
     public TagNotFoundException(String name) {
-        super("No tag with the name" + name + "was found");
+        super("No tag with the name " + name + " was found");
     }
 }
