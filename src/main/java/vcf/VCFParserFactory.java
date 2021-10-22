b666925ec084b54a2fcab27e1092d8eba33c760e
Love <love.lindqvist101@gmail.com>
2021-10-22 13:58:22 +0200

    Makes singleton getter synchronized

--- src/main/java/vcf/VCFParserFactory.java
+++ src/main/java/vcf/VCFParserFactory.java
@@ -3,11 +3,11 @@ package vcf;
 import model.ContactList;
 import model.TagHandler;
 
-public class VCFParserFactory {
+public final class VCFParserFactory {
 
     static private IVCFParser instance = null;
 
-    public static IVCFParser getService(ContactList contactList, TagHandler tagHandler) {
+    public static synchronized IVCFParser getService(ContactList contactList, TagHandler tagHandler) {
         if (instance == null) {
             instance = new VCFParser(contactList, tagHandler);
         }
