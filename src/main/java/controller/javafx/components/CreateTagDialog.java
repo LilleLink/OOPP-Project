b7ebce99c268ea9b4de6e147476d7040c0c1968f
Love <love.lindqvist101@gmail.com>
2021-10-23 16:44:14 +0200

    Removes duplicate '#'

--- src/main/java/controller/javafx/components/CreateTagDialog.java
+++ src/main/java/controller/javafx/components/CreateTagDialog.java
@@ -50,7 +50,7 @@ class CreateTagDialog extends ViewComponent {
     private void addTag() {
         try {
             Color color = colorPicker.getValue();
-            String formattedColor = String.format("#%02X%02X%02X",
+            String formattedColor = String.format("%02X%02X%02X",
                     (int) (color.getRed() * 255),
                     (int) (color.getGreen() * 255),
                     (int) (color.getBlue() * 255));
